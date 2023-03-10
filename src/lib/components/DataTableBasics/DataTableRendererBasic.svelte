4c783f0085ab33b444590fe1806a74f235b43a7c
BostoenToby <toby.bostoen@student.howest.be>
2023-03-10 12:51:34 +0100

    add color when mapped

--- src/lib/components/DataTableBasics/DataTableRendererBasic.svelte
+++ src/lib/components/DataTableBasics/DataTableRendererBasic.svelte
@@ -240,6 +240,7 @@
           {#if data.data.length < $pagination.rowsPerPage}
             {#each Array(data.data.length) as _, i}
               <tr
+                id={String(i + $pagination.rowsPerPage * ($pagination.currentPage - 1))}
                 on:click={function () {
                   if (rowEvent != null && updating == false && editClick == false) rowEvent(event, true)
                   editClick = false
@@ -267,6 +268,7 @@
           {:else}
             {#each Array($pagination.totalRows - $pagination.rowsPerPage * $pagination.currentPage > 0 ? $pagination.rowsPerPage : $pagination.rowsPerPage - ($pagination.rowsPerPage * $pagination.currentPage - $pagination.totalRows)) as _, i}
               <tr
+                id={String(i + $pagination.rowsPerPage * ($pagination.currentPage - 1))}
                 on:click={function () {
                   if (rowEvent != null && updating == false && editClick == false) rowEvent(event, true)
                   editClick = false
