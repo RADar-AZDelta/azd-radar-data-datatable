697899d8a34f5cd89746ec37fd07b31bda776269
Lammertyn Pieter-Jan <pjlammertyn@gmail.com>
2023-04-04 13:35:30 +0200

    removed sorting classes added

--- src/lib/components/DataTableBasics/Sorting.svelte
+++ src/lib/components/DataTableBasics/Sorting.svelte
@@ -1,46 +1,51 @@
 <script lang="ts">
-  import type { Nullable } from 'vitest'
   import SortDirection from '../../classes/enums/SortDirection'
   import SvgIcon from './SvgIcon.svelte'
+  import { createEventDispatcher } from 'svelte'
+
   export let col: string, direction: number, updateSorting: Function
 
   type HEX = `#${string}`
   let upColor: HEX, downColor: HEX
-  //let clazz: string
+  const dispatch = createEventDispatcher()
 
   $: switch (direction) {
     case SortDirection.Ascending:
       upColor = '#888888'
       downColor = '#ffffff'
-      //clazz = 'asc'
       break
     case SortDirection.Descending:
       upColor = '#ffffff'
       downColor = '#888888'
-      //clazz = 'desc'
       break
-    default:
+    case SortDirection.None:
       upColor = '#888888'
       downColor = '#888888'
-      //clazz = ''
       break
+  }
+
+  function onClick() {
+    // switch (direction) {
+    //   case SortDirection.Ascending:
+    //     direction = SortDirection.Descending
+    //     break
+    //   case SortDirection.Descending:
+    //     direction = SortDirection.None
+    //     break
+    //   case SortDirection.None:
+    //     direction = SortDirection.Ascending
+    //     break
+    // }
+    // if (direction == 2) direction = 0
+    // else direction++
+    dispatch('sortchanged', { col, direction })
   }
 </script>
 
-<button class="button is-small is-fullwidth" on:click={updateSorting(col, direction)}>
+<button class="button is-small is-fullwidth" on:click={/*onClick*/ updateSorting(col, direction)}>
   <p>{col}</p>
   <SvgIcon href="icons.svg" id="updown" width="1rem" height="1rem" --up-color={upColor} --down-color={downColor} />
-  <!--SvgIcon href="icons.svg" id="updown" width="1rem" height="1rem" class="updown {clazz}" /-->
 </button>
 
 <style>
-  /* :global(svg.updown.asc) {
-    --up-color: #888888;
-    --down-color: #ffffff;
-  }
-
-  :global(svg.updown.desc) {
-    --up-color: #ffffff;
-    --down-color: #888888;
-  } */
 </style>
