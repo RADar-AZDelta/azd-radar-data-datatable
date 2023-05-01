47bc5c2f967fda127effbefc56534977065ca7c0
Lammertyn Pieter-Jan <pjlammertyn@gmail.com>
2023-05-01 22:17:31 +0200

    solved onclick error

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -9,7 +9,7 @@
 
   const dispatch = createEventDispatcher<CustomTableEvents>()
 
-  function onClickOutside() {
+  function close() {
     dispatch('settingsVisibilityChanged', { visibility: false })
   }
 </script>
@@ -17,8 +17,8 @@
 {#if show}
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <section data-component="modal-container-medium" in:fade out:fade>
-    <div data-component="modal" use:clickOutside on:outclick={onClickOutside}>
-      <button data-component="close" on:click={onClick}
+    <div data-component="modal" use:clickOutside on:outclick={close}>
+      <button data-component="close" on:click={close}
         ><SvgIcon href="icons.svg" id="x" height="16px" width="16px" /></button
       >
       <div data-component="modal-content"><slot /></div>
