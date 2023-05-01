09e201278527ad64d82d6b02983b143613f0c08c
Lammertyn Pieter-Jan <pjlammertyn@gmail.com>
2023-05-01 10:39:51 +0200

    added fade to modal

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -2,18 +2,21 @@
   import { createEventDispatcher } from 'svelte'
   import SvgIcon from './SvgIcon.svelte'
   import type { CustomTableEvents } from './DataTable'
-  export let show: boolean
+  import { fade } from 'svelte/transition'
+
+  export let show: boolean = false
 
   const dispatch = createEventDispatcher<CustomTableEvents>()
 
-  function onClick() {
+  function onClick(event: MouseEvent) {
     dispatch('settingsVisibilityChanged', { visibility: false })
   }
 </script>
 
-{#if show == true}
-  <section data-component="modal-container-medium">
-    <div data-component="modal">
+{#if show}
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <section data-component="modal-container-medium" on:click|stopPropagation={onClick} in:fade out:fade>
+    <div data-component="modal" on:click|stopPropagation>
       <button data-component="close" on:click={onClick}
         ><SvgIcon href="icons.svg" id="x" height="16px" width="16px" /></button
       >
