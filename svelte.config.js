0b180db1fa670e207cc97eae37e77776104f7296
BostoenToby <toby.bostoen@student.howest.be>
2023-05-23 16:59:39 +0200

    add svelte inspector to datatable

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,25 @@
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'
 
-const dev = process.argv.includes('dev');
+const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [
-    vitePreprocess(),
-  ],
+  preprocess: [vitePreprocess()],
+  vitePlugin: {
+    inspector: {
+      toggleKeyCombo: 'control-shift',
+    },
+  },
   kit: {
-    adapter: adapter({
-    }),
+    adapter: adapter({}),
     paths: {
       base: dev ? '' : process.env.BASE_PATH,
-    }
+    },
   },
-  package: {
-  }
+  package: {},
 }
 
 export default config
