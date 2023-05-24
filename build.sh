231a0c5aa2aa24f514f9da2efff84ddb3c382893
BostoenToby <toby.bostoen@student.howest.be>
2023-05-24 15:55:00 +0200

    bump version 0.0.29

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# Set a new version in package.json
+# If this is your first time publishing, you will need to create an account with npmjs.com
+# run pnpm adduser (npm adduser if you are using npm) and follow the prompts
 rm -fr ./package
 pnpm run package
 pnpm publish ./package
